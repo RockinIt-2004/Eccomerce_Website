8825344bf9cb2c6fab92c245c0357274e66e6898
Dhruv <dhruv.2125cs1182@kiet.edu>
2024-05-19 02:01:43 +0530

    g

--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
-import os
+
 import sys
 
 
@@ -20,3 +20,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+# fvjhfvjhf
